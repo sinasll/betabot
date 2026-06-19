d768cbbcb813e6d9c81e1b7a3fbbe2a87d8c83d0
sinasll <sinasalah6@gmail.com>
2026-06-19 15:37:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,41 +1,48 @@
 const { Telegraf } = require('telegraf');
+const express = require('express');
 
-// لێرە توکێنا بۆتێ خوە ژ @BotFather وەرگرە
-const bot = new Telegraf('YOUR_TELEGRAM_BOT_TOKEN');
+// ۱. دروستکرنا وێب سێرڤەری بۆ هندێ Render بزانێت بۆت یێ کار دکەت
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-// سڵاڤکرن و دەستپێک
+app.get('/', (req, res) => {
+  res.send('بۆتێ IMPOSTER یێ ساخە! 🚀');
+});
+
+app.listen(PORT, () => {
+  console.log(`سێرڤەر ل سەر پۆرتێ ${PORT} دەستپێکر`);
+});
+
+// ۲. وەرگرتنا توکێنێ ژ سێرڤەرێ Render
+const BOT_TOKEN = process.env.BOT_TOKEN;
+
+if (!BOT_TOKEN) {
+  console.error('❌ خەلەتی: BOT_TOKEN د ناڤ Environment Variables دا نەیێ هەیە!');
+  process.exit(1);
+}
+
+const bot = new Telegraf(BOT_TOKEN);
+
+// ٣. تەنێ بەرسڤدانا فرمانا /start ب زمانێ بادینییا دهۆکێ
 bot.start((ctx) => {
-    ctx.reply('سڵاڤ! بخێر بێن بۆ یارییا IMPOSTER.\n' +
-              'دێ چەوا یاریێ کەی؟\n' +
-              '/start - دەستپێکرنا یاریێ\n' +
-              '/help - چەوانیا یاریێ\n' +
-              '/score - ڕێزبەندییا خالان');
+  ctx.reply(
+    'سڵاڤ! بخێر بێی بۆ یارییا فەرمی یا IMPOSTER 🎭\n\n' +
+    '📌 **چەوانیا دەستپێکرن و ڤەکرنا یاریێ:**\n\n' +
+    '١. ل خوارێ کلیلێ ل سەر دوگمەیا **"Play Game"** یان **"ڤەکرنا یاریێ"** بکە (یا ل تەنیشتا جهێ نڤیسینێ).\n' +
+    '٢. مینی ئەپ (Mini App) دێ بۆ تە ڤەبیت.\n' +
+    '٣. ناڤێن هەڤالێن خۆ زێدە بکە و ئێکسەر دەست ب کەیف و یاریێ بکەن! 🔥'
+  );
 });
 
-// چەوانیا یاریێ
-bot.command('help', (ctx) => {
-    ctx.reply('یاسا سادەنە:\n' +
-              '١. هەر کەسەک دێ پەیڤەکێ وەرگریت، ژبلی فێلبازی.\n' +
-              '٢. ب نۆرە وەسف بکەن.\n' +
-              '٣. فێلباز دێ هەول دەت خۆ ڤەشێریت.\n' +
-              '٤. د کۆتاییێ دا دەنگ بدەن بۆ فێلبازی!');
-});
+// دەستپێکرنا بۆتی
+bot.launch()
+  .then(() => {
+    console.log('بۆتێ IMPOSTER ب بادینی ل سەر Render دەست ب کار بوو! ✅');
+  })
+  .catch((err) => {
+    console.error('خەلەتی د دەستپێکرنێ دا:', err);
+  });
 
-// وەرگرتنا ئاستێ خالان
-bot.command('score', (ctx) => {
-    // لێرە دێ شیای خالێن د Store.js دا هەیین بخوینی
-    ctx.reply('هێشتا چ خال نینن، یاریێ دەستپێبکە داکو ناڤێ تە بچیتە د لیستێ دا!');
-});
-
-// وەرگرتنا فرمانێن یاریێ
-bot.command('play', (ctx) => {
-    ctx.reply('یاری دهێتە ئامادەکرن... ئەرێ تو دگەل ستافێ دهۆکێ یە؟');
-});
-
-bot.launch();
-
-// بۆ راوەستاندنا بۆتی ب سەلامەتی
+// ڕاوەستاندنا پاراستی
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
-
-console.log('بۆتێ IMPOSTER ب بادینی دەستپێکر...');
